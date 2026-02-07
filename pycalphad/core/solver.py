b74466fb533f51102b474e63718d8b639c78aab8
Marcus Bannerman <m.bannerman@gmail.com>
2026-02-07 21:12:45 +0000

    Add state initialization in solver method

--- pycalphad/core/solver.py
+++ pycalphad/core/solver.py
@@ -164,6 +164,7 @@ class Solver(SolverBase):
             print(f"Solver: Attempting to solve system at conditions {conditions} with starting point: {composition_sets}")
         spec = self.get_system_spec(composition_sets, conditions)
         self._fix_state_variables_in_compsets(composition_sets, conditions)
+        state = spec.get_new_state(composition_sets)
         converged = spec.run_loop(state, 10000)
 
         if self.remove_metastable:
